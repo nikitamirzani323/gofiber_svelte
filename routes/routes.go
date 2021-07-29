ea1b7f3ecca22761ba46d7c92f14665799f3bfe9
nikitamirzani323 <nikitamirzani323@gmail.com>
2021-07-29 17:57:24 +0700

    SVELTE

--- routes/routes.go
+++ routes/routes.go
@@ -5,7 +5,7 @@ import "github.com/gofiber/fiber/v2"
 func Init() *fiber.App {
 	app := fiber.New()
 
-	app.Static("/", "public")
+	app.Static("/", "client/public")
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{
 			"status":  fiber.StatusOK,
