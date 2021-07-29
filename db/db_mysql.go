d3c8d78882fc2353942baf1f4dbd3d62d6aee6e5
nikitamirzani323 <nikitamirzani323@gmail.com>
2021-07-29 17:28:13 +0700

    DEPEDENCY THANOS

--- db/db_mysql.go
+++ db/db_mysql.go
@@ -2,11 +2,16 @@ package db
 
 import (
 	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/nikitamirzani323/gofiber_svelte/configs"
 )
 
 var db *sql.DB
 var err error
 
 func InitMysql() {
+	conf := configs.ConfMysql()
+	connectionString := conf.DB_USERNAME + ":" + conf.DB_PASSWORD + "@tcp(" + conf.DB_HOST + ":" + conf.DB_PORT + ")/" + conf.DB_NAME
 
 }
