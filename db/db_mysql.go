db522ff40e26a9de9413ce82796526e808a1d61a
nikitamirzani323 <nikitamirzani323@gmail.com>
2021-07-29 17:29:58 +0700

    PACKAGE DB MYSQL

--- db/db_mysql.go
+++ db/db_mysql.go
@@ -11,7 +11,20 @@ var db *sql.DB
 var err error
 
 func InitMysql() {
-	conf := configs.ConfMysql()
+	conf := configs.GetConfMysql()
 	connectionString := conf.DB_USERNAME + ":" + conf.DB_PASSWORD + "@tcp(" + conf.DB_HOST + ":" + conf.DB_PORT + ")/" + conf.DB_NAME
 
+	db, err = sql.Open("mysql", connectionString)
+	if err != nil {
+		panic("koneksi string error")
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic("DNS invalid")
+	}
+}
+
+func CreateCon() *sql.DB {
+	return db
 }
