a7488b0f0c768354d4550ae1a91d14d27d3857af
nikitamirzani323 <nikitamirzani323@gmail.com>
2021-07-29 17:36:16 +0700

    new routes get

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/nikitamirzani323/gofiber_svelte/db"
+	"github.com/nikitamirzani323/gofiber_svelte/routes"
+)
+
+func main() {
+	db.InitMysql()
+	app := routes.Init()
+
+	log.Fatal(app.Listen(":4000"))
 }
---
+++ routes/routes.go
@@ -0,0 +1,15 @@
+package routes
+
+import "github.com/gofiber/fiber/v2"
+
+func Init() *fiber.App {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"status":  fiber.StatusOK,
+			"message": "fiber svelte",
+		})
+	})
+	return app
+}
